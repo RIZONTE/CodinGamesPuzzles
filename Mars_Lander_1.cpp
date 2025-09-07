054aa9edc97737684314661a0623f5e0b668c074
Eremin Danil <danchchik@bk.ru>
2025-09-07 12:27:27 +0500

    Mars Lander - Episode 1

---
+++ Mars_Lander_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+    int surface_n; // the number of points used to draw the surface of Mars.
+    std::cin >> surface_n;
+    std::cin.ignore();
+    for (int i = 0; i < surface_n; i++) {
+        int land_x; // X coordinate of a surface point. (0 to 6999)
+        int land_y; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        std::cin >> land_x >> land_y;
+        std::cin.ignore();
+    }
+
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int h_speed; // the horizontal speed (in m/s), can be negative.
+        int v_speed; // the vertical speed (in m/s), can be negative.
+        int fuel; // the quantity of remaining fuel in liters.
+        int rotate; // the rotation angle in degrees (-90 to 90).
+        int power; // the thrust power (0 to 4).
+        std::cin >> x >> y >> h_speed >> v_speed >> fuel >> rotate >> power;
+        std::cin.ignore();
+
+        if(v_speed <= -40){
+            std::cout << "0 4" << std::endl;
+        }
+        else{
+            std::cout << "0 0" << std::endl;
+        }
+    }
+}
